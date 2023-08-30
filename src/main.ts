655076df0a8c16ba8b911867d485218dd38df4e4
Nailson-araujo <nailson.araujo@activesoft.com.br>
2023-08-30 20:43:33 -0300

    criacao da sala do terceiro andar

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ WA.onInit().then(() => {
     WA.room.area.onEnter('clock').subscribe(() => {
         const today = new Date();
         const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
+        currentPopup = WA.ui.openPopup("clockPopup", "São " + time, []);
     })
 
     WA.room.area.onLeave('clock').subscribe(closePopup)
